23406dcf5b349fd183ff15eca8065aa7fd8f6c4d
Krissada Jindanupajit <jindanupajit@gmail.com>
2019-10-29 14:00:52 -0400

    Easter Eggs

--- src/ElizaApplication.java
+++ src/ElizaApplication.java
@@ -5,6 +5,11 @@ public class ElizaApplication {
     static HashMap<String, String> wordList = new HashMap<>();
     static ArrayList<String> hedges= new ArrayList<>();
     static ArrayList<String> qualifiers = new ArrayList<>();
+    static ArrayList<String> vowelEnding = new ArrayList<>();
+    static boolean pigMode = false;
+    static boolean capMode = false;
+    static boolean redMode = false;
+    static ArrayList<String> log = new ArrayList<>();
 
     public static void main(String[] args) {
         init();
@@ -25,6 +30,10 @@ public class ElizaApplication {
         qualifiers.add("Why do you say that");
         qualifiers.add("You seem to think that");
         qualifiers.add("So, you are concerned that");
+
+        vowelEnding.add("way");
+        vowelEnding.add("tay");
+        vowelEnding.add("yay");
     }
 
     public static void run() {
@@ -35,41 +44,79 @@ public class ElizaApplication {
         do {
             System.out.print("Enter your response here or Q to quit: ");
             input = nextLine();
-            if (quitRequested = input.equalsIgnoreCase("Q"))
+            log.add("  <you> "+input);
+            if (quitRequested = input.equalsIgnoreCase("Q")) {
+                log.add("*** Quitting");
                 break;
+            }
 
-            System.out.println(
-                    ((new Random()).nextInt(2) == 0)?
-                        RespondWithHedge(input):
-                        RespondWithQualifier(input)
-                    );
+            if (input.equalsIgnoreCase("pig")) {
+                pigMode = true;
+                System.out.println("Notice: "+pig("PIG LATIN MODE"));
+                log.add("*** Pig mode enabled");
+                continue;
+            }
+
+            if (input.equalsIgnoreCase("cap")) {
+                capMode = true;
+                System.out.println("Notice: CAP MODE");
+                log.add("*** Cap mode enabled");
+                continue;
+            }
+
+            if (input.equalsIgnoreCase("red")) {
+                redMode = true;
+                System.out.println("Notice: "+"\u001B[91m"+"RED MODE!"+"\u001B[0m");
+                log.add("*** Red mode enabled");
+                continue;
+            }
+
+            if (input.equalsIgnoreCase("play game")) {
+                redMode = true;
+                System.out.println("Notice: Play game!");
+                log.add("*** play game");
+                GameApplication.main(null);
+                System.out.println("Notice: Game end!");
+                continue;
+            }
+
+
+            String response = ((new Random()).nextInt(2) == 0)?
+                    RespondWithHedge(input):
+                    RespondWithQualifier(input);
+
+            if (capMode) response = response.toUpperCase();
+
+            if (redMode) response = "\u001B[91m"+response+"\u001B[0m";
+
+            log.add("<Eliza> "+response);
+            System.out.println(response);
         }
         while(true);
 
         System.out.println(">>> END");
+        System.out.println("=== Chat log ===");
+        for (String line : log) {
+            System.out.println(line);
+        }
+
     }
 
     public static String RespondWithHedge(String input) {
-        return hedges.get((new Random()).nextInt(hedges.size()));
+        String response =  hedges.get((new Random()).nextInt(hedges.size()));
+        return (pigMode?pig(response):response);
     }
 
     public static String RespondWithQualifier(String input) {
-        return qualifiers.get((new Random()).nextInt(qualifiers.size()))
+        String response =   qualifiers.get((new Random()).nextInt(qualifiers.size()))
                 +" "+replace(input);
+        return (pigMode?pig(response):response) ;
     }
 
     public static String replace(String input) {
-
-        ArrayList<String> replaced =  replace(tokenization(input));
-        StringBuilder newString = new StringBuilder();
-        while(!replaced.isEmpty()) {
-            newString.append(replaced.remove(0));
-            if (!replaced.isEmpty()) {
-                newString.append(" ");
-            }
-        }
-        return newString.toString();
+        return arrayToString(replace(tokenization(input)));
     }
+
     public static ArrayList<String> replace(ArrayList<String> input) {
         ArrayList<String> newString = new ArrayList<>();
         for (String token : input) {
@@ -88,11 +135,79 @@ public class ElizaApplication {
         return newString;
     }
 
+
+    public static String pig(String input) {
+        return arrayToString(pig(tokenization(input)));
+    }
+
+    public static ArrayList<String> pig(ArrayList<String> input) {
+        ArrayList<String> newString = new ArrayList<>();
+        for (String token : input) {
+            String replacement = pigReplaceWord(token);
+            newString.add(replacement);
+        }
+        return newString;
+    }
+
+    private static String pigReplaceWord(String word) {
+
+        if (word.length() <= 0) {
+            return "";
+        }
+
+
+        String newWord = swapAtFirstVowel(word);
+
+        if (newWord.equals(word))
+            return vowel(word);
+        else
+            return newWord + "ay";
+
+    }
+
+    private static String vowel(String word) {
+
+        return word+vowelEnding.get((new Random()).nextInt(vowelEnding.size()));
+
+    }
+
     private static String nextLine() {
         return kb.nextLine();
     }
 
     private static ArrayList<String> tokenization(String input) {
         return new ArrayList<String>(Arrays.asList(input.split(" ")));
+    }
+
+    private static String swapAtFirstVowel(String input) {
+        if (input.length() <= 0) return "";
+        ArrayList<String> vowelList = new ArrayList<>(Arrays.asList("A","a","E","e","I","i","O","o","U","u"));
+        int index = -1;
+
+        for (String v : vowelList) {
+            index = input.indexOf(v);
+            if (index > 0) {
+                return input.substring(index) + input.substring(0,index);
+            }
+            else if (index == 0) {
+                break;
+            }
+        }
+
+        return input;
+    }
+
+    private static String arrayToString(ArrayList<String> input) {
+        return arrayToString(input, " ");
+    }
+    private static String arrayToString(ArrayList<String> input, String  separator) {
+        StringBuilder newString = new StringBuilder();
+        while(!input.isEmpty()) {
+            newString.append(input.remove(0));
+            if (!input.isEmpty()) {
+                newString.append(separator);
+            }
+        }
+        return newString.toString();
     }
 }
---
+++ src/GameApplication.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameApplication {
+    public static void main(String[] args) {
+        System.out.println("Choose your own adventure");
+        System.out.print("    You found a dragon, do you want to kill him ? (yes/no): ");
+        Scanner kb = new Scanner(System.in);
+        switch(kb.nextLine().toLowerCase()) {
+            case "yes":
+                System.out.println("You die!");
+                break;
+            case "no":
+                System.out.println("Congratulations!, you got a pet dragon");
+                break;
+            default:
+                System.out.println("You are nut!, try again!");
+        }
+    }
+}
